eaa500689d6f4b5bfc17091665c29ab954205f9f
Renan Araújo <111506459+renantfm4@users.noreply.github.com>
2023-03-06 11:49:22 -0300

    Update Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -1,16 +1,20 @@
 #Importações
+from plotly.graph_objects import Figure
+from dash import Dash, dcc, html, ctx
+from dash.dependencies import Input, Output
+from dash import dcc
+import dash_daq as daq
 import pandas as pd
-from pandas.core.frame import DataFrame
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px 
+import plotly_express as px
 
-app = Dash(__name__, title = 'Criptomoedas')
+
+app = Dash(__name__)
 
 #---------------------------------------------------------------------------------------------------------
 #Grafico 1
 
 #Ler .csv
-df = pd.read_csv('coin_Bitcoin.csv')
+df = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/coin_Bitcoin.csv')
 df_array = df.values
 
 #Gráfico de linha por dia
@@ -24,7 +28,7 @@ for linha in df_array:
 fig1 = px.line(
     x=dias,
     y=marketcap,
-)    
+)
 
 #Gráfico de linha por ano
 
@@ -32,7 +36,7 @@ fig1 = px.line(
 contador = 0
 anos = []
 while contador < 2991:
-  dias[contador] = dias[contador].split('-')  
+  dias[contador] = dias[contador].split('-')
   anos.append(dias[contador][0])
   contador = contador+1
 
@@ -76,7 +80,7 @@ fig2 = px.line(
 )
 
 marketdic2 =	{
-  '2013': [], 
+  '2013': [],
   '2014': [],
   '2015': [],
   '2016': [],
@@ -98,8 +102,8 @@ while c < 2991:
 #---------------------------------------------------------------------------------------------------------
 #Grafico 2
 
-df = pd.read_csv('coin_Bitcoin.csv')
-df1 = pd.read_csv('O_brabo.csv')
+df = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/coin_Bitcoin.csv')
+df1 = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/O_brabo.csv')
 
 #transforma o Bitcoin.csv em lista
 lista_bitcoin = df.values.tolist()
@@ -183,7 +187,7 @@ plot2 = px.line(y=price_bitcoin, x=ano_bitcoin, title='Crypto moedas',
 #---------------------------------------------------------------------------------------------------------
 #Grafico 3
 
-df_1 = pd.read_csv('AllCoin.csv')
+df_1 = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/AllCoin.csv')
 df_array_1 = df_1.values
 
 anos1 = []
@@ -203,9 +207,9 @@ graph_volume= px.histogram(lista_volume, x=anos1, y=volume, color=name)
 #---------------------------------------------------------------------------------------------------------
 #Grafico 4
 
-df_btc = pd.read_csv('coin_Bitcoin.csv')
+df_btc = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/coin_Bitcoin.csv')
 df_btc_array = df_btc.values
-df_eth = pd.read_csv('coin_Ethereum.csv')
+df_eth = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/coin_Ethereum.csv')
 df_eth_array = df_eth.values
 
 marketcap_btc = []
@@ -236,7 +240,7 @@ graph_marketcap = px.line(x=df_marketcap['data'], y=media_marketcap)
 #Grafico 5
 
 
-df_moedas = pd.read_csv('https://raw.githubusercontent.com/Diaxiz/Python-UnB/main/Criptomoedas.csv')
+df_moedas = pd.read_csv('/Users/benjamim/PycharmProjects/DashAPC/venv/data/Criptomoedas.csv')
 
 media_preco = df_moedas.values.tolist()
 
@@ -254,7 +258,7 @@ valores_media = []
 for coluna in media_preco:
   valores_media.append([((coluna[4]+coluna[5])/2)*Real,coluna[2],coluna[3]])
 
-moedas = DataFrame(valores_media, columns=['Media Preco Dia','Moeda', 'Data'])
+moedas = pd.DataFrame(valores_media, columns=['Media Preco Dia','Moeda', 'Data'])
 
 fig = px.line(moedas, x='Data', y='Media Preco Dia', color='Moeda')
 
@@ -262,90 +266,95 @@ fig = px.line(moedas, x='Data', y='Media Preco Dia', color='Moeda')
 #Layout do Dash
 
 app.layout = html.Div(children=[
-        html.H1(children='Criptomoedas',),
-        html.Div(children=[  
-          html.H3(children='Grupo B da turma 12')
-        ]),
-        html.Div(children=[
+    html.H1(children='Criptomoedas', ),
+    html.Div(children=[
+        html.H3(children='Grupo B da turma 12')
+    ]),
+    html.Div(children=[
         html.H4(children='Marketcap do Bitcoin ao longo dos anos'),
-          dcc.Dropdown(anos, value= None , id='anos')
-        ]),
-        html.Div(children=[
-          dcc.Graph(
-              id= 'Gráfico_Bitcoin',
-              figure= fig1 
-          )
-        ]),
-        html.Div(children=[
-          html.H4(children='Comparação entre Bitcoin e dolár'),
-          dcc.Dropdown(['Gráfico Dólar', 'Gráfico Bitcoin'], value='Gráfico Dólar', id='Moedas')
-        ]),
-        html.Div(children=[
-            dcc.Graph(
-                id='graph',
-                figure= plot1
-          )
-        ]),
-        html.Div(children=[
-            html.H4(children='Volume de algumas criptomoedas'),
-            dcc.Dropdown(
-                id = 'dropdown',
-                options=[{'label': i, 'value':i} for i in avaliable_names],value= None, multi = False, placeholder = 'Filtre as moedas'
-          )
-        ]),
-        html.Div(children=[
-          dcc.Graph(
-            id = 'graph-volume'
-          )
-        ]),
-        html.Div(children=[
-            html.H4(children='Marketcap do Bitcoin e Ethereum'),
-            dcc.Graph(
-                id = 'graph-marketcap'
-          )
-        ]),
-        html.Div(children=[
-          dcc.Slider(
-            id = "slider",
-            min= df_marketcap['year'].min(),
-            max = df_marketcap['year'].max(),
+        dcc.Dropdown(anos, value=None, id='anos')
+    ]),
+    html.Div(children=[
+        dcc.Graph(
+            id='Gráfico_Bitcoin',
+            figure=fig1
+        )
+    ]),
+    html.Div(children=[
+        html.H4(children='Comparação entre Bitcoin e dolár'),
+        dcc.Dropdown(['Gráfico Dólar', 'Gráfico Bitcoin'], value='Gráfico Dólar', id='Moedas')
+    ]),
+    html.Div(children=[
+        dcc.Graph(
+            id='graph',
+            figure=plot1
+        )
+    ]),
+    html.Div(children=[
+        html.H4(children='Volume de algumas criptomoedas'),
+        dcc.Dropdown(
+            id='dropdown',
+            options=[{'label': i, 'value': i} for i in avaliable_names], value=None, multi=False,
+            placeholder='Filtre as moedas'
+        )
+    ]),
+    html.Div(children=[
+        dcc.Graph(
+            id='graph-volume'
+        )
+    ]),
+    html.Div(children=[
+        html.H4(children='Marketcap do Bitcoin e Ethereum'),
+        dcc.Graph(
+            id='graph-marketcap'
+        )
+    ]),
+    html.Div(children=[
+        dcc.Slider(
+            id="slider",
+            min=df_marketcap['year'].min(),
+            max=df_marketcap['year'].max(),
             value=df_marketcap['year'].min(),
             marks=m,
             step=None
-          )
-        ]),
-        html.Div(children=[
-            html.H4(children='Valor de algumas criptomoedas em diferentes cotações'),
-            dcc.Dropdown(['Real', 'Libra', 'Euro', 'Kwanza', 'Yen'], value='Real', id='botao'),
-        ]),
-        html.Div(children=[
-          dcc.Graph(
+        )
+    ]),
+    html.Div(children=[
+        html.H4(children='Valor de algumas criptomoedas em diferentes cotações'),
+        dcc.Dropdown(['Real', 'Libra', 'Euro', 'Kwanza', 'Yen'], value='Real', id='botao'),
+    ]),
+    html.Div(children=[
+        dcc.Graph(
             id='grafico',
             figure=fig
-          )
-        ])
-        
+        )
+    ])
+
 ])
 
-#---------------------------------------------------------------------------------------------------------
-#Callbacks
+
+# ---------------------------------------------------------------------------------------------------------
+# Callbacks
 
 @app.callback(
     Output('Gráfico_Bitcoin', 'figure'),
     Input('anos', 'value')
 )
 def atualizar_output(value):
-    
     if value != None:
         for key in marketdic:
             if value == key:
                 fig1 = px.line(
-                    x = marketdic2[key],
-                    y = marketdic[key],
+                    x=marketdic2[key],
+                    y=marketdic[key],
                 )
-                
+        fig1.update_layout(xaxis=dict(title="Anos"),
+                           yaxis=dict(title="Média anual"))
+
         return fig1
     else:
+        fig2.update_layout(xaxis=dict(title="Anos"),
+                           yaxis=dict(title="Média anual"))
         return fig2
 
 
@@ -353,36 +362,42 @@ def atualizar_output(value):
     Output("graph", "figure"),
     Input("Moedas", "value")
 )
-def atualizar_output(value):  
+def atualizar_output(value):
     if value == 'Gráfico Dólar':
-      return plot1
+        plot1.update_layout(xaxis=dict(title="Anos"),
+                           yaxis=dict(title="Valor"))
+        return plot1
     elif value == 'Gráfico Bitcoin':
-      return plot2
+        plot2.update_layout(xaxis=dict(title="Anos"),
+                           yaxis=dict(title="Valor"))
+        return plot2
 
 
 @app.callback(
     Output('graph-volume', 'figure'),
     Input('dropdown', 'value'))
-
 def update_volume(value):
     ts = df_1[df_1["Name"].isin([value])]
-    fig = px.histogram(ts, x="Date", y="Volume", color='Name' )
+    fig = px.histogram(ts, x="Date", y="Volume", color='Name')
     if value == None:
+        graph_volume.update_layout(xaxis=dict(title="Anos"),
+                           yaxis=dict(title="Valor do volume"))
         return graph_volume
+    fig.update_layout(xaxis=dict(title="Anos"),
+                       yaxis=dict(title="Valor Volume"))
     return fig
 
 
 @app.callback(
     Output('graph-marketcap', 'figure'),
     Input('slider', 'value'))
-
 def update_volume(selected_value):
-    #ts = df_marketcap[df_marketcap["year"].isin([value])]
+    # ts = df_marketcap[df_marketcap["year"].isin([value])]
     filtered = df_marketcap[df_marketcap.year == selected_value]
     fig = px.line(filtered, x=df_marketcap['year'], y=media_marketcap)
-    fig.update_layout(transition_duration=500)
+    fig.update_layout(xaxis=dict(title="Anos"),
+                       yaxis=dict(title="Marketcap Arrecadado"))
     return fig
-
 
 
 @app.callback(
@@ -394,27 +409,29 @@ def update_output(value):
 
         valores_media = []
 
-        #Pegando as colunas, tirando a média e convertendo pra real
+        # Pegando as colunas, tirando a média e convertendo pra real
 
         for coluna in media_preco:
-            valores_media.append([((coluna[4]+coluna[5])/2)*Real,coluna[2],coluna[3]])
+            valores_media.append([((coluna[4] + coluna[5]) / 2) * Real, coluna[2], coluna[3]])
 
-        moedas = DataFrame(valores_media, columns=['Media Preco Dia','Moeda', 'Data'])
+        moedas = pd.DataFrame(valores_media, columns=['Media Preco Dia', 'Moeda', 'Data'])
 
         fig = px.line(moedas, x='Data', y='Media Preco Dia', color='Moeda')
     else:
         valores_media = []
 
-        #Pegando as colunas, tirando a média e convertendo pra real
+        # Pegando as colunas, tirando a média e convertendo pra real
 
         for coluna in media_preco:
-            valores_media.append([((coluna[4]+coluna[5])/2)*valores[valores_2.index(value)],coluna[2],coluna[3]])
+            valores_media.append(
+                [((coluna[4] + coluna[5]) / 2) * valores[valores_2.index(value)], coluna[2], coluna[3]])
 
-        moedas = DataFrame(valores_media, columns=['Media Preco Dia','Moeda', 'Data'])
+        moedas = pd.DataFrame(valores_media, columns=['Media Preco Dia', 'Moeda', 'Data'])
 
         fig = px.line(moedas, x='Data', y='Media Preco Dia', color='Moeda')
     return fig
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#roda o app
+if __name__ == "__main__" :
+    app.run_server(debug = True)
